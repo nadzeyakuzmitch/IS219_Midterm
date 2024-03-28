ded3991afd8df504601ae468fc9a848f7969feed
Nadzeya Kuzmitch <nk83@njit.edu>
2024-03-28 09:19:17 -0500

    set up app skeleton

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+import os
+import pkgutil
+import importlib
+import sys
+from app.commands import CommandHandler, Command
+from dotenv import load_dotenv
+import logging
+import logging.config
+
+class App:
+    def __init__(self): 
+        # Constructor
+
+    def start(self):
+        # App start handler
