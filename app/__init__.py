f55131cb1fcb62ab058c9a2ce09ca34a94f1fc4b
Nadzeya Kuzmitch <nk83@njit.edu>
2024-03-28 09:52:55 -0500

    added commands initialization and registration

--- app/__init__.py
+++ app/__init__.py
@@ -8,8 +8,28 @@ import logging
 import logging.config
 
 class App:
-    def __init__(self): 
+    def __init__(self):
         # Constructor
 
+    def load_plugins(self):
+        # Dynamically load all plugins in the plugins directory
+        plugins_package = 'app.plugins'
+        for _, plugin_name, is_pkg in pkgutil.iter_modules([plugins_package.replace('.', '/')]):
+            if is_pkg:  # Ensure it's a package
+                plugin_module = importlib.import_module(f'{plugins_package}.{plugin_name}')
+                for item_name in dir(plugin_module):
+                    item = getattr(plugin_module, item_name)
+                    try:
+                        if issubclass(item, (Command)):  # Assuming a BaseCommand class exists
+                            self.command_handler.register_command(plugin_name, item())
+                            logging.info(f"Command '{plugin_name}' from plugin '{plugin_name}' registered.")
+                    except TypeError:
+                        continue  # If item is not a class or unrelated class, just ignore
     def start(self):
-        # App start handler
+        # Register commands here
+        self.load_plugins()
+        while True:  #REPL Read, Evaluate, Print, Loop
+            self.command_handler.execute_command(input(f'Type operation command or type `menu` for available commands (type `exit` to exit):\n{self.get_environment_variable()} >>> ').strip())
+
+
+
---
+++ app/commands/__init__.py
@@ -0,0 +1,32 @@
+from abc import ABC, abstractmethod
+
+class Command(ABC):
+    @abstractmethod
+    def description(self):
+        pass
+
+    @abstractmethod
+    def execute(self, commands_list):
+        pass
+
+class CommandHandler:
+    def __init__(self):
+        self.commands = {}
+
+    def register_command(self, command_name: str, command: Command):
+        self.commands[command_name] = command
+
+    def execute_command(self, command_name: str):
+        """ Look before you leap (LBYL) - Use when its less likely to work
+        if command_name in self.commands:
+            self.commands[command_name].execute()
+        else:
+            print(f"No such command: {command_name}")
+        """
+        """Easier to ask for forgiveness than permission (EAFP) - Use when its going to most likely work"""
+        try:
+            print(f"{command_name}")
+            self.commands[command_name].execute(self.commands)
+        except KeyError:
+            print(f"\n---------------\nNo such command: {command_name}\n---------------\n")
+
